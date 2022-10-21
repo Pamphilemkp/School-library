d33ff42be9e13da2eb5b1619a2af79e57def0618
Mark Otuya <markotuya0@gmail.com>
2022-10-21 23:26:18 +0100

    linters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,25 +1,24 @@
 require_relative '../person'
 
 describe('checking person details') do
-    context("should return the details about the person") do 
-        
-        person = Person.new(21, "John Doe", parent_permission: false)
+  context('should return the details about the person') do
+    person = Person.new(21, 'John Doe', parent_permission: false)
 
-         it 'Initialized with name, age and parent_permission' do
-            age = person.age
-            name = person.name
+    it 'Initialized with name, age and parent_permission' do
+      age = person.age
+      name = person.name
 
-            expect(person.parent_permission).to be_falsy
-            expect(age).to be(21)
-            expect(name).to eq("John Doe")
-          end
-
-       it("It should return true if the teacher has permission for the service") do
-          expect(person.can_use_services?).to be_falsy
-       end
-
-       it("It should return the correct name") do
-        expect(person.correct_name).to eq("John Doe")
-     end
+      expect(person.parent_permission).to be_falsy
+      expect(age).to be(21)
+      expect(name).to eq('John Doe')
     end
- end 
+
+    it('It should return true if the teacher has permission for the service') do
+      expect(person.can_use_services?).to be_falsy
+    end
+
+    it('It should return the correct name') do
+      expect(person.correct_name).to eq('John Doe')
+    end
+  end
+end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -3,18 +3,17 @@ require_relative '../book'
 require_relative '../rental'
 
 describe('testing the decorator pattern') do
-  
-    person = Person.new(21, "Pamphile", parent_permission: false)
-    book = Book.new("48 laws of power", "Robert Green")
+  person = Person.new(21, 'Pamphile', parent_permission: false)
+  book = Book.new('48 laws of power', 'Robert Green')
 
-         it 'cinitialize the rentals with the date' do
-          rental = Rental.new("2022/10/22", person, book)
-          person_name = person.name
-          book_tile = book.title
-          rental_date = rental.date
+  it 'cinitialize the rentals with the date' do
+    rental = Rental.new('2022/10/22', person, book)
+    person_name = person.name
+    book_tile = book.title
+    rental_date = rental.date
 
-          expect(person_name).to eql("Pamphile")
-          expect(book_tile).to eql("48 laws of power")
-          expect(rental_date).to eql("2022/10/22")
-     end
- end 
+    expect(person_name).to eql('Pamphile')
+    expect(book_tile).to eql('48 laws of power')
+    expect(rental_date).to eql('2022/10/22')
+  end
+end
